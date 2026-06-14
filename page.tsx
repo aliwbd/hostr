f64beac1c73ee604a3b21c78c96cae31c364d0ea
Muhammad Ali <aliocp3@gmail.com>
2026-06-14 15:30:59 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,18 @@
+// import image from "\public\hoston_logo.jpeg";
+// import logo from '/public/hoston_logo.jpeg'; // Adjust path
+
+import image from "\public\hoston_logo.jpeg";
+export default function Home() {
+  return (
+    <div>
+      <h1>Houston Easy Repairs</h1>
+      <img src="/hoston_logo.jpeg" width="200" height="100" alt="description"alt="Description" />;
+    </div>
+  );
+
+              ;
+              
+}
+
+
+
